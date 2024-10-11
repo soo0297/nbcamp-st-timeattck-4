12228df662f294dbfa528f735690d63751e7a787
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-10-11 12:36:36 +0900

    isrTest 페이지

--- src/app/isrTest/page.tsx
+++ src/app/isrTest/page.tsx
@@ -1,5 +1,22 @@
-const isrTestPage = () => {
-  return <div>isrTestPage</div>;
-};
-
-export default isrTestPage;
+export async function getAdviceData() {
+  const res = await fetch(
+    "https://korean-advice-open-api.vercel.app/api/advice",
+    {
+      next: {
+        revalidate: 10,
+      },
+    }
+  );
+  const data = res.json();
+  return data;
+}
+export default async function ssgTestPage() {
+  const data = await getAdviceData();
+  return (
+    <div>
+      <h1>{data.author}</h1>
+      <p>{data.authorProfile}</p>
+      <p>{data.message}</p>
+    </div>
+  );
+}
