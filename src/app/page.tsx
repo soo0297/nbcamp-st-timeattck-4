a91509e6355f5e155911953d310ca4fb216c99f5
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-10-11 11:38:00 +0900

    페이지 링크 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Page() {
-  return <div></div>;
-}
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <div className="flex gap-4">
+      <Link href="/todos">Todos</Link>
+      <Link href="/sstTest">sstTest</Link>
+      <Link href="/isrTest">isrTest</Link>
+    </div>
+  );
+};
+
+export default page;
