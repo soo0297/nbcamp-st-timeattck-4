93fccce4a6a3fbbf85c3770e82043618b32b39bc
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-10-11 12:42:03 +0900

    todos페이지 작업

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,7 +1,47 @@
 "use client";
 
+import axios from "axios";
+import { useState } from "react";
+
 const todosPage = () => {
-  return <div>todosPage</div>;
+  const [title, setTitle] = useState("");
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleTitleInput = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleTodoInput = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const addTodos = () => {
+    axios.post("http://localhost:4000/todos", todoList);
+    setTitle("");
+    setTodo("");
+  };
+
+  return (
+    <>
+      <div className="flex">
+        <div>
+          <label>제목</label>
+          <input type="text" value={title} onChange={handleTitleInput} />
+        </div>
+        <div>
+          <label>할일</label>
+          <input type="text" value={todo} onChange={handleTodoInput} />
+        </div>
+        <button onClick={addTodos}>추가하기</button>
+      </div>
+      <div>
+        {todoList.map((todo) => {
+          return todo.title;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default todosPage;
