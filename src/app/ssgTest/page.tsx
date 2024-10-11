003cd23fe8d9cf9f59bb92dc91c9b94d958d6bb6
ssuyeong <107804563+soo0297@users.noreply.github.com>
2024-10-11 12:36:53 +0900

    ssgTest 페이지

--- src/app/ssgTest/page.tsx
+++ src/app/ssgTest/page.tsx
@@ -1,5 +1,14 @@
-const ssgTestPage = () => {
-  return <div>ssgTestPage</div>;
-};
-
-export default ssgTestPage;
+export async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts/10");
+  const data = res.json();
+  return data;
+}
+export default async function ssgTestPage() {
+  const data = await getData();
+  return (
+    <div>
+      <p>title: {data.title}</p>
+      <p>body: {data.body}</p>
+    </div>
+  );
+}
